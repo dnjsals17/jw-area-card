285bd1199dcf20598f09c9bd21b2c12e33d1633f
dnjsals17 <dnjsals17@naver.com>
2025-09-23 19:20:15 +0900

    .

--- excel/excel.js
+++ excel/excel.js
@@ -1,3 +1,6 @@
+import * as Excel from "exceljs/dist/exceljs.min.js";
+import { saveAs } from "file-saver";
+
 const express = require('express');
 const Excel = require('exceljs');
 const cors = require('cors');
@@ -37,11 +40,3 @@ app.post('/create-excel', async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-import * as Excel from "exceljs/dist/exceljs.min.js";
-import { saveAs } from "file-saver";
-
-// 여러 엑셀 시트를 포함하는 하나의 workbook(단위) 생성
-const wb = new Excel.Workbook();
-
-// 엑셀 sheet 생성
-const sheet = wb.addWorksheet("test");
